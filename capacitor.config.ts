63fe8ac31d3b47d4c9a5aacceb0c7f76e4f6aace
Sijibomi Olusunmbola <sijibomiolusunmbola@gmail.com>
2025-11-23 21:36:29 +0100

    chore(package): add haptics plugin to capacitor android config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.studio.everyday',
   appName: 'Everyday',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LocalNotifications: {
+      sound: 'gentle.wav',
+      channels: [
+        {
+          id: 'gentle-reminders',
+          name: 'Gentle Reminders',
+          importance: 3
+        }
+      ]
+    }
+  }
 };
 
 export default config;
